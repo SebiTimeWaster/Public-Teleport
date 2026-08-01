af222701d3af42a1120a800647e3b1a4debe8ca2
Time Waster <time@waster.com>
2026-08-02 01:44:14 +0200

    added commented out code

--- src/main/java/timewaster/publicteleport/Teleports.java
+++ src/main/java/timewaster/publicteleport/Teleports.java
@@ -4,14 +4,14 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
-// import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-// import net.minecraft.sounds.SoundEvents;
-// import net.minecraft.sounds.SoundSource;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.level.Level;
 import timewaster.publicteleport.records.Teleport;
 
@@ -22,17 +22,29 @@ public class Teleports {
         this.storage = storage;
     }
 
+    @SuppressWarnings("null")
     private static void doTeleportEffect(ServerLevel world, ServerPlayer player) {
         // TODO: fix sound and particles
-        // world.playSound(null, player.getBlockX() + 0.5, player.getBlockY() + 0.5,
-        // player.getBlockZ() + 0.5,
-        // SoundEvents.ENDERMAN_TELEPORT,
-        // SoundSource.PLAYERS, 1.0f, 1.0f);
+        world.playSound(
+            null,
+            player.getBlockX() + 0.5,
+            player.getBlockY() + 0.5,
+            player.getBlockZ() + 0.5,
+            SoundEvents.ENDERMAN_TELEPORT,
+            SoundSource.PLAYERS,
+            1.0f,
+            1.0f);
 
-        // world.sendParticles(ParticleTypes.PORTAL, player.getBlockX() + 0.5,
-        // player.getBlockY() + 0.5,
-        // player.getBlockZ() + 0.5, 25, 0.25,
-        // 0.25, 0.25, 0.0);
+        world.sendParticles(
+            ParticleTypes.PORTAL,
+            player.getBlockX() + 0.5,
+            player.getBlockY() + 0.5,
+            player.getBlockZ() + 0.5,
+            25,
+            0.25,
+            0.25,
+            0.25,
+            0.0);
     }
 
     private static boolean teleport(ServerPlayer player, Teleport target) {
